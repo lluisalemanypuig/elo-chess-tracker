d1c9542f70b13b744eca28deed7d4cb5e30f8e46
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2023-01-08 17:27:43 +0100

    Fixed minor bugs

--- ts-source/rating_system/Elo.ts
+++ ts-source/rating_system/Elo.ts
@@ -13,32 +13,37 @@ function rating_adjustment(exp_score: number, score: number, k: number): number 
 function update_classical(game: Game): [Player, Player] {
 	let white = game.white.clone();
 	let black = game.black.clone();
+	const result = game.result;
 
-	let white_rating = game.white.get_classical_rating();
-	let black_rating = game.black.get_classical_rating();
+	let white_rating = white.get_classical_rating();
+	let black_rating = black.get_classical_rating();
 
 	let exp_score_a = get_exp_score(white_rating.rating, black_rating.rating);
-	if (game.result == 'white_wins') {
+	if (result == 'white_wins') {
 		white_rating.rating += rating_adjustment(exp_score_a,     1,   white_rating.K);
 		black_rating.rating += rating_adjustment(1 - exp_score_a, 0,   black_rating.K);
 
 		white_rating.won += 1;
 		black_rating.lost += 1;
 	}	
-	else if (game.result == 'black_wins') {
+	else if (result == 'black_wins') {
 		white_rating.rating += rating_adjustment(exp_score_a,     0,   white_rating.K);
 		black_rating.rating += rating_adjustment(1 - exp_score_a, 1,   black_rating.K);
 
 		white_rating.lost += 1;
 		black_rating.won += 1;
 	}
-	else {
+	else if (result == 'draw') {
 		white_rating.rating += rating_adjustment(exp_score_a,     0.5, white_rating.K);
 		black_rating.rating += rating_adjustment(1 - exp_score_a, 0.5, black_rating.K);
 
 		white_rating.drawn += 1;
 		black_rating.drawn += 1;
 	}
+
+	// update White's and Black's number of games
+	++white_rating.num_games;
+	++black_rating.num_games;
 
 	// update White's constant
 	if (white_rating.num_games >= 30) {
@@ -55,6 +60,9 @@ function update_classical(game: Game): [Player, Player] {
 	if (black_rating.rating >= 2400) {
 		black_rating.K = 10;
 	}
+
+	white.set_classical_rating(white_rating);
+	black.set_classical_rating(black_rating);
 
 	return [white, black];
 }
