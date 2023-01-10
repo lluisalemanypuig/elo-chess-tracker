431df35e835204339f5af3f32817043a8e80ec55
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2023-01-10 21:09:52 +0100

    Create directory structure

--- restart_test_database.sh
+++ restart_test_database.sh
@@ -2,8 +2,14 @@
 
 rm -rf database/*
 mkdir -p database/
+mkdir -p database/users
+mkdir -p database/games
+mkdir -p database/challenges
 cp -r database-test/* database/
 
 rm -rf test/database/*
 mkdir -p test/database
+mkdir -p test/database/users
+mkdir -p test/database/games
+mkdir -p test/database/challenges
 cp -r database-test/* test/database/
