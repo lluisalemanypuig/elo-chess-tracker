0c311831c651149599315a3c9692af8af67c2cc7
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2023-08-11 18:00:19 +0200

    Polished the interface for editing a game's result

--- ts-source/client_games_list.ts
+++ ts-source/client_games_list.ts
@@ -30,16 +30,62 @@ function edit_button_was_clicked(event: any) {
 	console.log(`Button ${event.target.id} was clicked!`);
 }
 
-function new_button_cell(text: string, id: string, row_num: number) {
-	var button = document.createElement("button");
+function new_button_cell(text: string, game_id: string, row_num: number) {
+	let button = document.createElement("button") as HTMLButtonElement;
 	button.textContent = text;
+	button.id = "button-" + game_id + "-" + row_num;
+	button.setAttribute("game_id", game_id);
+	button.setAttribute("row_num", String(row_num));
 	button.onclick = edit_button_was_clicked;
-	button.id = "button-" + id + "-" + row_num;
+	button.disabled = true;
 	return button;
 }
 
-window.onload = async function () {
+function result_selection_changed(event: any) {
+	const select = event.target;
+	const id = select.id as string;
+	
+	let button = document.getElementById( select.getAttribute("button_id") ) as HTMLButtonElement;
 
+	if (select.value != select.getAttribute("original_value")) {
+		button.disabled = false;
+	}
+	else {
+		button.disabled = true;
+	}
+}
+
+function new_select_cell_result(original_result: string, game_id: string, row_num: number) {
+	let select_result = document.createElement("select") as HTMLSelectElement;
+
+	const result_from_text_to_value = function(text: string) {
+		if (text == "1 - 0") { return "white_wins"; }
+		if (text == "1/2 - 1/2") { return "draw"; }
+		if (text == "0 - 1") { return "black_wins"; }
+		return "????";
+	}
+
+	{
+	const add_result_option = function(text: string) {
+		let option_result = document.createElement("option") as HTMLOptionElement;
+		option_result.text = text;
+		option_result.value = result_from_text_to_value(text);
+		select_result.appendChild(option_result);
+	};
+	add_result_option("1 - 0");
+	add_result_option("1/2 - 1/2");
+	add_result_option("0 - 1");
+	}
+
+	select_result.value = result_from_text_to_value(original_result);
+	select_result.onchange = result_selection_changed;
+	select_result.setAttribute("original_value", result_from_text_to_value(original_result));
+	select_result.setAttribute("button_id", "button-" + game_id + "-" + row_num);
+
+	return select_result;
+}
+
+window.onload = async function () {
 	const val = document.getElementById("type_of_list")?.getAttribute('value');
 
 	let query_to_server: string = "";
@@ -75,7 +121,9 @@ window.onload = async function () {
 		row.appendChild(new_text_cell(games[i].id));
 		row.appendChild(new_text_cell(games[i].white));
 		row.appendChild(new_text_cell(games[i].black));
-		row.appendChild(new_text_cell(games[i].result));
+		
+		row.appendChild(new_select_cell_result(games[i].result, games[i].id, i + 1));
+		
 		row.appendChild(new_text_cell(games[i].time_control));
 		row.appendChild(new_text_cell(games[i].date));
 		row.appendChild(new_text_cell(games[i].white_rating));
@@ -84,7 +132,7 @@ window.onload = async function () {
 		row.appendChild(new_text_cell(games[i].black_increment));
 
 		if (games[i].editable == "yes") {
-			row.appendChild(new_button_cell("Edit", games[i].id, i+1));
+			row.appendChild(new_button_cell("Edit", games[i].id, i + 1));
 		}
 
 		tbody.appendChild(row);
