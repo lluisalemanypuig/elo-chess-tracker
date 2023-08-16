e093f7e4750d563d9dc7c1e6c2c721d5aa3722ea
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2023-08-16 19:01:55 +0200

    Fixed minor bug

--- ts-source/client_challenges.ts
+++ ts-source/client_challenges.ts
@@ -32,7 +32,7 @@ async function fill_username_datalist() {
 
 	// "query" the server
 	const response = await fetch(
-		"/query_user_list",
+		"/query_users_list",
 		{
 			method: 'GET',
 			headers: { 'Content-type': 'application/json; charset=UTF-8' }
