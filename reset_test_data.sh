fb9aeb2859f4a0ca382575cd27508711ab918ea9
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2024-10-26 10:52:01 +0200

    Regression

--- reset_test_data.sh
+++ reset_test_data.sh
@@ -5,4 +5,8 @@
 rm -rf test/webpage/*
 cp -r webpage-test/* test/webpage/
 
+# these directories may not be created in the 'cp -r' above
+mkdir -p test/webpage/databse/challenges
+mkdir -p test/webpage/databse/games
+
 mv test/webpage/configuration_test.json test/webpage/configuration.json
