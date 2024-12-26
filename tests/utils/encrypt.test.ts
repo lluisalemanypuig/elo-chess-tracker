56e0114d8987b5fe2eb8c83ce6f3955ad55f81f1
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2024-12-26 13:04:45 +0100

    [tests] Test encryption using Kanji

--- tests/utils/encrypt.test.ts
+++ tests/utils/encrypt.test.ts
@@ -27,19 +27,83 @@ import {
 	is_password_of_user_correct
 } from '../../ts-server/utils/encrypt';
 
-function check_encrypt_user_password(user: string, pass: string) {
-	const [encrypted, iv] = encrypt_password_for_user(user, pass);
-	expect(is_password_of_user_correct(encrypted, user, pass, iv)).toBe(true);
-}
-
 function check_decrypt_good_password(msg: string, pass: string) {
 	const enc = encrypt_message(msg, pass);
 	expect(decrypt_message(enc, pass)).toBe(msg);
 }
 
+describe('Encryption and decryption of messages with a (correct) plain password', () => {
+	test('1', () => {
+		check_decrypt_good_password('', 'admin');
+	});
+
+	test('2', () => {
+		check_decrypt_good_password('as', 'admin');
+	});
+
+	test('3', () => {
+		check_decrypt_good_password('asdf fqrfwrf', 'admin');
+	});
+
+	test('4', () => {
+		check_decrypt_good_password('QW  2424guhgnj gk rfr', 'admin');
+	});
+
+	test('5', () => {
+		check_decrypt_good_password('admin', '星');
+	});
+
+	test('6', () => {
+		check_decrypt_good_password('山田', '星');
+	});
+
+	test('7', () => {
+		check_decrypt_good_password('山田', '山田');
+	});
+});
+
 function check_decrypt_wrong_password(msg: string, pass1: string, pass2: string) {
 	const enc = encrypt_message(msg, pass1);
 	expect(decrypt_message(enc, pass2)).not.toBe(msg);
+}
+
+describe('Encryption and decryption with a (wrong) plain password', () => {
+	test('1', () => {
+		const enc1 = encrypt_message('', 'admin');
+
+		expect(decrypt_message(enc1, 'admin!')).toBe('');
+		expect(decrypt_message(enc1, 'admi')).toBe('');
+	});
+
+	test('2', () => {
+		check_decrypt_wrong_password('a', 'admin', 'admin!');
+		check_decrypt_wrong_password('a', 'admin', 'admi');
+	});
+
+	test('3', () => {
+		check_decrypt_wrong_password('as', 'admin', 'admin!');
+		check_decrypt_wrong_password('as', 'admin', 'admi');
+	});
+
+	test('4', () => {
+		check_decrypt_wrong_password('asdf fqrfwrf', 'admin', 'admin!');
+		check_decrypt_wrong_password('asdf fqrfwrf', 'admin', 'admi');
+	});
+
+	test('5', () => {
+		check_decrypt_wrong_password('QW  2424guhgnj gk rfr', 'admin', 'admin!');
+		check_decrypt_wrong_password('QW  2424guhgnj gk rfr', 'admin', 'admi');
+	});
+
+	test('5', () => {
+		check_decrypt_wrong_password('QW  2424guhgnj gk rfr', 'admin', '山田');
+		check_decrypt_wrong_password('QW  2424guhgnj gk rfr', 'admin', '星');
+	});
+});
+
+function check_encrypt_user_password(user: string, pass: string) {
+	const [encrypted, iv] = encrypt_password_for_user(user, pass);
+	expect(is_password_of_user_correct(encrypted, user, pass, iv)).toBe(true);
 }
 
 describe('Encrypt password for users', () => {
@@ -64,52 +128,5 @@ describe('Encrypt password for users', () => {
 		for (const user of user_array) {
 			check_encrypt_user_password(user, 'QQQQQQQ');
 		}
-	});
-});
-
-describe('Encryption and decryption of messages with a (correct) plain password', () => {
-	test('1', () => {
-		check_decrypt_good_password('', 'admin');
-	});
-
-	test('2', () => {
-		check_decrypt_good_password('as', 'admin');
-	});
-
-	test('3', () => {
-		check_decrypt_good_password('asdf fqrfwrf', 'admin');
-	});
-
-	test('4', () => {
-		check_decrypt_good_password('QW  2424guhgnj gk rfr', 'admin');
-	});
-});
-
-describe('Encryption and decryption with a (wrong) plain password', () => {
-	test('1', () => {
-		const enc1 = encrypt_message('', 'admin');
-
-		expect(decrypt_message(enc1, 'admin!')).toBe('');
-		expect(decrypt_message(enc1, 'admi')).toBe('');
-	});
-
-	test('2.1', () => {
-		check_decrypt_wrong_password('a', 'admin', 'admin!');
-		check_decrypt_wrong_password('a', 'admin', 'admi');
-	});
-
-	test('2', () => {
-		check_decrypt_wrong_password('as', 'admin', 'admin!');
-		check_decrypt_wrong_password('as', 'admin', 'admi');
-	});
-
-	test('3', () => {
-		check_decrypt_wrong_password('asdf fqrfwrf', 'admin', 'admin!');
-		check_decrypt_wrong_password('asdf fqrfwrf', 'admin', 'admi');
-	});
-
-	test('4', () => {
-		check_decrypt_wrong_password('QW  2424guhgnj gk rfr', 'admin', 'admin!');
-		check_decrypt_wrong_password('QW  2424guhgnj gk rfr', 'admin', 'admi');
 	});
 });
