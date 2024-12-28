915e4b76cf29b21da37a974be731efd2f09f935b
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2024-12-28 16:26:22 +0100

    [tests] Added small tests for generic Rating

---
+++ tests/rating_framework/rating.test.ts
@@ -0,0 +1,36 @@
+/*
+Elo rating for a Chess Club
+Copyright (C) 2023 - 2024  Lluís Alemany Puig
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Contact:
+	Lluís Alemany Puig
+	https://github.com/lluisalemanypuig
+*/
+
+import { Rating } from '../../ts-server/rating_framework/rating';
+
+describe('clone', () => {
+	test('1', () => {
+		const r = new Rating(1500.0, 100, 50, 30, 20);
+		const rc = r.clone();
+
+		expect(rc.rating).toBe(r.rating);
+		expect(rc.num_games).toBe(r.num_games);
+		expect(rc.won).toBe(r.won);
+		expect(rc.drawn).toBe(r.drawn);
+		expect(rc.lost).toBe(r.lost);
+	});
+});
