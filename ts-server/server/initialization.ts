a973b746a894d187b72ab0a380c05cf2b303bc60
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2024-12-29 12:00:19 +0100

    [ts-server] Minor changes

--- ts-server/server/initialization.ts
+++ ts-server/server/initialization.ts
@@ -39,6 +39,104 @@ import { TimeControl } from '../models/time_control';
 import { player_vs_player as Elo } from '../rating_framework/Elo/formula';
 import { Elo_rating_from_json, Elo_rating_new } from '../rating_framework/Elo/rating';
 
+function initialize_directories(base_directory: string): void {
+	ServerEnvironment.get_instance().set_database_base_directory(path.join(base_directory, '/database'));
+	debug(log_now(), `    Database directory: '${ServerEnvironment.get_instance().get_dir_database()}'`);
+	debug(log_now(), `        Games directory: '${ServerEnvironment.get_instance().get_dir_games()}'`);
+	debug(log_now(), `        Users directory: '${ServerEnvironment.get_instance().get_dir_users()}'`);
+	debug(log_now(), `        Challenges directory: '${ServerEnvironment.get_instance().get_dir_challenges()}'`);
+}
+
+function initialize_SSL_files(base_directory: string, configuration_data: any): void {
+	ServerEnvironment.get_instance().set_SSL_info(
+		path.join(base_directory, '/ssl'),
+		configuration_data.ssl_certificate.public_key_file,
+		configuration_data.ssl_certificate.private_key_file,
+		configuration_data.ssl_certificate.passphrase_file
+	);
+	debug(log_now(), `    SSL base directory: '${ServerEnvironment.get_instance().get_dir_ssl()}'`);
+	debug(log_now(), `        Public key file: '${ServerEnvironment.get_instance().get_ssl_public_key_file()}'`);
+	debug(log_now(), `        Private key file: '${ServerEnvironment.get_instance().get_ssl_private_key_file()}'`);
+	debug(log_now(), `        Passphrase: '${ServerEnvironment.get_instance().get_ssl_passphrase_file()}'`);
+}
+
+function initialize_server_ports(configuration_data: any): void {
+	let server_configuration = ServerConfiguration.get_instance();
+	server_configuration.set_port_http(configuration_data.ports.http);
+	server_configuration.set_port_https(configuration_data.ports.https);
+}
+
+function initialize_icon_file_paths(base_directory: string, configuration_data: any): void {
+	ServerEnvironment.get_instance().set_icons_info(
+		path.join(base_directory, '/icons'),
+		'/' + configuration_data.favicon,
+		'/' + configuration_data.login_page.icon,
+		'/' + configuration_data.home_page.icon
+	);
+}
+
+function initialize_page_titles(configuration_data: any): void {
+	ServerEnvironment.get_instance().set_titles_info(
+		configuration_data.login_page.title,
+		configuration_data.home_page.title
+	);
+}
+
+function initialize_permissions(permission_data: any): void {
+	debug(log_now(), 'Initialize permissions...');
+
+	let actions = UserRoleToUserAction.get_instance();
+
+	// ADMIN
+	for (let i = 0; i < permission_data.admin.length; ++i) {
+		actions.add_to_role(ADMIN, permission_data.admin[i]);
+	}
+	// TEACHER
+	for (let i = 0; i < permission_data.teacher.length; ++i) {
+		actions.add_to_role(TEACHER, permission_data.teacher[i]);
+	}
+	// MEMBER
+	for (let i = 0; i < permission_data.member.length; ++i) {
+		actions.add_to_role(MEMBER, permission_data.member[i]);
+	}
+	// STUDENT
+	for (let i = 0; i < permission_data.student.length; ++i) {
+		actions.add_to_role(STUDENT, permission_data.student[i]);
+	}
+}
+
+function initialize_rating_framework(rating_type: string): void {
+	debug(log_now(), `    Rating system: '${rating_type}'`);
+	let rating_system = RatingSystem.get_instance();
+	if (rating_type == 'Elo') {
+		rating_system.set_rating_formula(Elo);
+		rating_system.set_rating_from_JSON_formula(Elo_rating_from_json);
+		rating_system.set_new_rating(Elo_rating_new);
+	} else {
+		debug(log_now(), `Invalid rating system '${rating_type}'`);
+	}
+}
+
+function initialize_time_controls(time_control_array: any): void {
+	debug(log_now(), 'Initialize time controls...');
+
+	let all_time_controls: TimeControl[] = [];
+	for (var time_control in time_control_array) {
+		const tc = time_control_array[time_control];
+		all_time_controls.push(new TimeControl(tc.id, tc.name));
+	}
+
+	let rating_system = RatingSystem.get_instance();
+	rating_system.set_time_controls(all_time_controls);
+	rating_system.make_unique_time_controls();
+
+	debug(log_now(), `    Found '${all_time_controls.length}' rating types:`);
+	for (let i = 0; i < all_time_controls.length; ++i) {
+		debug(log_now(), `        * Id '${all_time_controls[i].id}'`);
+		debug(log_now(), `          Name '${all_time_controls[i].name}'`);
+	}
+}
+
 function initialize_sessions(): void {
 	debug(log_now(), 'Initialize sessions...');
 
@@ -133,105 +231,6 @@ function initialize_games(): void {
 	debug(log_now(), `    Maximum game id ${max_game_id}.`);
 }
 
-function initialize_permissions(permission_data: any): void {
-	debug(log_now(), 'Initialize permissions...');
-
-	let actions = UserRoleToUserAction.get_instance();
-
-	// ADMIN
-	for (let i = 0; i < permission_data.admin.length; ++i) {
-		actions.add_to_role(ADMIN, permission_data.admin[i]);
-	}
-	// TEACHER
-	for (let i = 0; i < permission_data.teacher.length; ++i) {
-		actions.add_to_role(TEACHER, permission_data.teacher[i]);
-	}
-	// MEMBER
-	for (let i = 0; i < permission_data.member.length; ++i) {
-		actions.add_to_role(MEMBER, permission_data.member[i]);
-	}
-	// STUDENT
-	for (let i = 0; i < permission_data.student.length; ++i) {
-		actions.add_to_role(STUDENT, permission_data.student[i]);
-	}
-}
-
-function initialize_time_controls(time_control_array: any): void {
-	debug(log_now(), 'Initialize time controls...');
-
-	let all_time_controls: TimeControl[] = [];
-	for (var time_control in time_control_array) {
-		let tc = time_control_array[time_control];
-		all_time_controls.push(new TimeControl(tc.id, tc.name));
-	}
-
-	let rating_system = RatingSystem.get_instance();
-	rating_system.set_time_controls(all_time_controls);
-	rating_system.make_unique_time_controls();
-
-	debug(log_now(), `    Found '${all_time_controls.length}' rating types:`);
-	for (let i = 0; i < all_time_controls.length; ++i) {
-		debug(log_now(), `        * Id '${all_time_controls[i].id}'`);
-		debug(log_now(), `          Name '${all_time_controls[i].name}'`);
-	}
-}
-
-function initialize_directories(base_directory: string): void {
-	ServerEnvironment.get_instance().set_database_base_directory(path.join(base_directory, '/database'));
-	debug(log_now(), `    Database directory: '${ServerEnvironment.get_instance().get_dir_database()}'`);
-	debug(log_now(), `        Games directory: '${ServerEnvironment.get_instance().get_dir_games()}'`);
-	debug(log_now(), `        Users directory: '${ServerEnvironment.get_instance().get_dir_users()}'`);
-	debug(log_now(), `        Challenges directory: '${ServerEnvironment.get_instance().get_dir_challenges()}'`);
-}
-
-function initialize_SSL_files(base_directory: string, configuration_data: any): void {
-	ServerEnvironment.get_instance().set_SSL_info(
-		path.join(base_directory, '/ssl'),
-		configuration_data.ssl_certificate.public_key_file,
-		configuration_data.ssl_certificate.private_key_file,
-		configuration_data.ssl_certificate.passphrase_file
-	);
-	debug(log_now(), `    SSL base directory: '${ServerEnvironment.get_instance().get_dir_ssl()}'`);
-	debug(log_now(), `        Public key file: '${ServerEnvironment.get_instance().get_ssl_public_key_file()}'`);
-	debug(log_now(), `        Private key file: '${ServerEnvironment.get_instance().get_ssl_private_key_file()}'`);
-	debug(log_now(), `        Passphrase: '${ServerEnvironment.get_instance().get_ssl_passphrase_file()}'`);
-}
-
-function initialize_icon_file_paths(base_directory: string, configuration_data: any): void {
-	ServerEnvironment.get_instance().set_icons_info(
-		path.join(base_directory, '/icons'),
-		'/' + configuration_data.favicon,
-		'/' + configuration_data.login_page.icon,
-		'/' + configuration_data.home_page.icon
-	);
-}
-
-function initialize_page_titles(configuration_data: any): void {
-	ServerEnvironment.get_instance().set_titles_info(
-		configuration_data.login_page.title,
-		configuration_data.home_page.title
-	);
-}
-
-function initialize_rating_system(configuration_data: any): void {
-	const rating_type = configuration_data.rating_system;
-	debug(log_now(), `    Rating system: '${rating_type}'`);
-	let rating_system = RatingSystem.get_instance();
-	if (rating_type == 'Elo') {
-		rating_system.set_rating_formula(Elo);
-		rating_system.set_rating_from_JSON_formula(Elo_rating_from_json);
-		rating_system.set_new_rating(Elo_rating_new);
-	} else {
-		debug(log_now(), `Invalid rating system '${rating_type}'`);
-	}
-}
-
-function initialize_server_ports(configuration_data: any): void {
-	let server_configuration = ServerConfiguration.get_instance();
-	server_configuration.set_port_http(configuration_data.ports.http);
-	server_configuration.set_port_https(configuration_data.ports.https);
-}
-
 export function server_initialize_from_data(base_directory: string, configuration_data: any): void {
 	debug(log_now(), `    Base directory: '${base_directory}'`);
 
@@ -240,8 +239,8 @@ export function server_initialize_from_data(base_directory: string, configuratio
 	initialize_server_ports(configuration_data);
 	initialize_icon_file_paths(base_directory, configuration_data);
 	initialize_page_titles(configuration_data);
-	initialize_rating_system(configuration_data);
 	initialize_permissions(configuration_data.permissions);
+	initialize_rating_framework(configuration_data.rating_system);
 	initialize_time_controls(configuration_data.time_controls);
 
 	initialize_sessions();
@@ -261,7 +260,7 @@ export function server_initialize_from_configuration_file(configuration_file: st
 	server_initialize_from_data(base_path, json_data);
 }
 
-export function server_initialize_from_default_configuration_file(args: string[]): void {
+export function server_initialize_from_parameters(args: string[]): void {
 	let configuration_file: string = '';
 	for (let i = 0; i < args.length; ++i) {
 		if (args[i] == 'configuration-file') {
