e4493c7223f5302314c311e540709f66f1e059fc
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2024-12-29 11:12:50 +0100

    [tests] Added tests for session id

---
+++ tests/models/session_id.test.ts
@@ -0,0 +1,29 @@
+/*
+Elo rating for a Chess Club
+Copyright (C) 2023 - 2024  Lluís Alemany Puig
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Contact:
+    Lluís Alemany Puig
+    https://github.com/lluisalemanypuig
+*/
+
+import { SessionID } from '../../ts-server/models/session_id';
+
+test('Fields contents are correct', () => {
+	const sid = new SessionID('asdf', 'qwer');
+	expect(sid.id).toEqual('asdf');
+	expect(sid.username).toEqual('qwer');
+});
