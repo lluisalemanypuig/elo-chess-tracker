f3d55bc6ca18e05411604a77280249b1ced892db
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2025-01-01 19:29:04 +0100

    [ts-server] Minor changes

--- ts-server/utils/user_relationships.ts
+++ ts-server/utils/user_relationships.ts
@@ -44,59 +44,39 @@ import { ADMIN, MEMBER, STUDENT, TEACHER, UserRole } from '../models/user_role';
 export function can_user_edit(editor: User, edited: User): boolean {
 	return (
 		editor.can_do(EDIT_USER) &&
-		((edited.is(ADMIN) && editor.can_do(EDIT_ADMIN)) ||
-			(edited.is(TEACHER) && editor.can_do(EDIT_TEACHER)) ||
-			(edited.is(MEMBER) && editor.can_do(EDIT_MEMBER)) ||
-			(edited.is(STUDENT) && editor.can_do(EDIT_STUDENT)))
+		((editor.can_do(EDIT_ADMIN) && edited.is(ADMIN)) ||
+			(editor.can_do(EDIT_TEACHER) && edited.is(TEACHER)) ||
+			(editor.can_do(EDIT_MEMBER) && edited.is(MEMBER)) ||
+			(editor.can_do(EDIT_STUDENT) && edited.is(STUDENT)))
 	);
 }
 
 /// Can a user (@e u) see a game between two players (@e white and @e black)?
 export function can_user_see_a_game(u: User, white: User, black: User): boolean {
-	if (!u.can_do(SEE_USER_GAMES)) {
-		return false;
-	}
-
 	const either_user_is = function (r: UserRole): boolean {
 		return white.is(r) || black.is(r);
 	};
 
-	if (u.can_do(SEE_ADMIN_GAMES) && either_user_is(ADMIN)) {
-		return true;
-	}
-	if (u.can_do(SEE_TEACHER_GAMES) && either_user_is(TEACHER)) {
-		return true;
-	}
-	if (u.can_do(SEE_STUDENT_GAMES) && either_user_is(STUDENT)) {
-		return true;
-	}
-	if (u.can_do(SEE_MEMBER_GAMES) && either_user_is(MEMBER)) {
-		return true;
-	}
-	return false;
+	return (
+		u.can_do(SEE_USER_GAMES) &&
+		((u.can_do(SEE_ADMIN_GAMES) && either_user_is(ADMIN)) ||
+			(u.can_do(SEE_TEACHER_GAMES) && either_user_is(TEACHER)) ||
+			(u.can_do(SEE_STUDENT_GAMES) && either_user_is(STUDENT)) ||
+			(u.can_do(SEE_MEMBER_GAMES) && either_user_is(MEMBER)))
+	);
 }
 
 /// Can a user (@e u) edit a game between two players (@e white and @e black)?
 export function can_user_edit_a_game(u: User, white: User, black: User): boolean {
-	if (!u.can_do(EDIT_USER_GAMES)) {
-		return false;
-	}
-
 	const either_user_is = function (r: UserRole): boolean {
 		return white.is(r) || black.is(r);
 	};
 
-	if (u.can_do(EDIT_ADMIN_GAMES) && either_user_is(ADMIN)) {
-		return true;
-	}
-	if (u.can_do(EDIT_TEACHER_GAMES) && either_user_is(TEACHER)) {
-		return true;
-	}
-	if (u.can_do(EDIT_STUDENT_GAMES) && either_user_is(STUDENT)) {
-		return true;
-	}
-	if (u.can_do(EDIT_MEMBER_GAMES) && either_user_is(MEMBER)) {
-		return true;
-	}
-	return false;
+	return (
+		u.can_do(EDIT_USER_GAMES) &&
+		((u.can_do(EDIT_ADMIN_GAMES) && either_user_is(ADMIN)) ||
+			(u.can_do(EDIT_TEACHER_GAMES) && either_user_is(TEACHER)) ||
+			(u.can_do(EDIT_STUDENT_GAMES) && either_user_is(STUDENT)) ||
+			(u.can_do(EDIT_MEMBER_GAMES) && either_user_is(MEMBER)))
+	);
 }
