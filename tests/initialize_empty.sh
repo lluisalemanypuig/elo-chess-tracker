6fc8860e5d7ec4f411055bc2b119c5dd1b0c69c1
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2025-01-04 10:49:57 +0100

    [tests] Added helper script

---
+++ tests/initialize_empty.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -rf tests/webpage
+
+mkdir -p tests/webpage/database/challenges
+mkdir -p tests/webpage/database/users
+mkdir -p tests/webpage/database/games
+mkdir -p tests/webpage/icons
+mkdir -p tests/webpage/ssl
+
