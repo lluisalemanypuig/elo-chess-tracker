d8898dea33ba643e7c5d692ff86cfb892e51b9e8
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2025-01-05 10:40:44 +0100

    [tests] Added a utility function for running bash commands

---
+++ tests/server/exec_utils.ts
@@ -0,0 +1,38 @@
+/*
+Elo rating for a Chess Club
+Copyright (C) 2023 - 2025  Lluís Alemany Puig
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Full source code of elo-chess-tracker:
+	https://github.com/lluisalemanypuig/elo-chess-tracker
+
+Contact:
+	Lluís Alemany Puig
+	https://github.com/lluisalemanypuig
+*/
+
+import { exec } from 'child_process';
+
+export function run_command(command: string): Promise<void> {
+	return new Promise((resolve, reject) => {
+		exec(command, (error, _stdout, _stderr) => {
+			if (error) {
+				reject(error);
+				return;
+			}
+			resolve();
+		});
+	});
+}
