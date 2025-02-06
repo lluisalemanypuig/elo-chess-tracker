877de45e83b0d71f2f317837a1970ad2268a3f81
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2025-02-06 18:43:22 +0100

    [src-server] Several changes
    - Added functions to read a graph from a directory
    - Simplified class definition
    - Added functions to write a graph into different files

--- src-server/models/graph/graph.ts
+++ src-server/models/graph/graph.ts
@@ -23,36 +23,30 @@ Contact:
 	https://github.com/lluisalemanypuig
 */
 
+import path from 'path';
+import fs from 'fs';
+
 import { EdgeMetadata } from './edge_metadata';
-import { Edge, edge_from_json } from './edge';
+import { Edge, edge_set_from_json } from './edge';
 import { search_by_key, where_should_be_inserted_by_key } from '../../utils/searching';
-import { Game, GameResult } from '../game';
+import { GameResult } from '../game';
+
+export type Neighborhood = Edge[];
 
 /**
  * @brief Graph abstraction for games between pairs of users.
  */
 export class Graph {
 	/// This is used to locate the games for any user A
-	private user_to_index: Map<string, number> = new Map();
-	private max_user_idx: number = 0;
-
-	/// The list of games for every user.
-	private adjacency_list: Edge[][] = [];
+	private adjacency_list: Map<string, Neighborhood> = new Map();
 
 	/// Add a new user to the graph.
 	private push_user(user: string): void {
-		this.user_to_index.set(user, this.max_user_idx);
-		++this.max_user_idx;
+		this.adjacency_list.set(user, []);
 	}
 
-	/**
-	 * @brief Adds a new edge to this neighborhood.
-	 *
-	 * This is a sorted insertion by target.
-	 * @param neigh The new edge to add.
-	 */
-	add_game(game: Game): void {
-		this.add_edge(game.get_white(), game.get_black(), game.get_result());
+	get_entries(): MapIterator<string> {
+		return this.adjacency_list.keys();
 	}
 
 	add_edge(W: string, B: string, result: GameResult): void {
@@ -61,23 +55,28 @@ export class Graph {
 	}
 
 	add_edge_raw(W: string, edge: Edge): void {
-		let white_idx = this.user_to_index.get(W);
-		if (white_idx == undefined) {
-			white_idx = this.max_user_idx;
+		let _W_list = this.adjacency_list.get(W);
+		if (_W_list == undefined) {
 			this.push_user(W);
-			this.adjacency_list.push([]);
+			_W_list = this.adjacency_list.get(W);
 		}
 
-		let W_list = this.adjacency_list[white_idx];
+		let W_list = _W_list as Neighborhood;
 
 		const [edge_idx, exists]: [number, boolean] = where_should_be_inserted_by_key(
 			W_list,
 			edge.neighbor,
-			function (s: string, t: string): number {
-				return s.localeCompare(t);
-			},
 			function (a: Edge) {
 				return a.neighbor;
+			},
+			function (s: string, t: string): number {
+				if (s < t) {
+					return -1;
+				}
+				if (s > t) {
+					return 1;
+				}
+				return 0;
 			}
 		);
 
@@ -89,79 +88,90 @@ export class Graph {
 	}
 
 	get_data(W: string, B: string): EdgeMetadata | undefined {
-		const W_idx = this.user_to_index.get(W);
-		if (W_idx == undefined) {
+		const _W_list = this.adjacency_list.get(W);
+		if (_W_list == undefined) {
 			return undefined;
 		}
 
-		const W_list = this.adjacency_list[W_idx];
+		const W_list = _W_list as Neighborhood;
 		const B_idx = search_by_key(
 			W_list,
 			B,
-			function (a: string, b: string) {
-				return a.localeCompare(b);
-			},
 			function (a: Edge) {
 				return a.neighbor;
+			},
+			function (s: string, t: string) {
+				if (s < t) {
+					return -1;
+				}
+				if (s > t) {
+					return 1;
+				}
+				return 0;
 			}
 		);
 		return B_idx == -1 ? undefined : W_list[B_idx].metadata;
 	}
 
 	get_degree(u: string): number {
-		const u_idx = this.user_to_index.get(u);
-		if (u_idx == undefined) {
+		const _u_list = this.adjacency_list.get(u);
+		if (_u_list == undefined) {
 			return -1;
 		}
-		return this.adjacency_list[u_idx].length;
+		return (_u_list as Neighborhood).length;
 	}
 
-	get_edges(u: string): Edge[] | undefined {
-		const u_idx = this.user_to_index.get(u);
-		if (u_idx == undefined) {
-			return undefined;
-		}
-		return this.adjacency_list[u_idx];
+	get_outgoing_edges(u: string): Neighborhood | undefined {
+		return this.adjacency_list.get(u);
 	}
 
 	get_oponents(u: string): string[] | undefined {
-		const u_idx = this.user_to_index.get(u);
-		if (u_idx == undefined) {
+		const _u_list = this.adjacency_list.get(u);
+		if (_u_list == undefined) {
 			return undefined;
 		}
-		return this.adjacency_list[u_idx].map((e: Edge): string => {
+		const u_list = _u_list as Neighborhood;
+		return u_list.map((e: Edge): string => {
 			return e.neighbor;
 		});
 	}
 }
 
+export function neighborhood_to_file(dir: string, username: string, edges: Neighborhood): void {
+	const filename = path.join(dir, username);
+	fs.writeFileSync(filename, JSON.stringify(edges, null, 4));
+}
+
+export function graph_to_file(dir: string, changes: string[], g: Graph): void {
+	for (const username of changes) {
+		neighborhood_to_file(dir, username, g.get_outgoing_edges(username) as Neighborhood);
+	}
+}
+
+export function graph_full_to_file(dir: string, g: Graph): void {
+	graph_to_file(dir, Array.from(g.get_entries()), g);
+}
+
 /**
  * @brief Parses a JSON string or object and returns a Graph.
- * @param json A string with data of a Graph.
+ * @param dir The directory where to read the graph from.
  * @returns A new Graph object.
  * @pre If @e json is a string, then it cannot start with '['.
  */
-export function graph_from_json(json: any): Graph {
-	if (typeof json === 'string') {
-		const json_parse = JSON.parse(json);
-		return graph_from_json(json_parse);
-	}
+export function graph_from_json(dir: string): Graph {
+	let g = new Graph();
 
-	const user_to_index = json.user_to_index;
-	let index_to_user: string[] = new Array(user_to_index.length);
-	for (const [key, value] of user_to_index) {
-		index_to_user[value] = key;
-	}
+	const files = fs.readdirSync(dir);
+	for (let i = 0; i < files.length; ++i) {
+		const username = files[i] as string;
 
-	let G = new Graph();
-	const adj = json['adjacency_list'];
-	for (let i = 0; i < adj.length; ++i) {
-		const W = index_to_user[i];
-		for (let j = 0; j < adj[i].length; ++j) {
-			const edge = edge_from_json(adj[i][j]);
-			G.add_edge_raw(W, edge);
+		const filename = path.join(dir, username);
+		const edge_set = edge_set_from_json(filename);
+
+		for (let i = 0; i < edge_set.length; ++i) {
+			g.add_edge_raw(username, edge_set[i]);
 		}
 	}
 
-	return G;
+	return g;
 }
