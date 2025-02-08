2f6e59a6ea762dfc65eeabb42321b80ad7f30a18
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2025-02-08 17:37:43 +0100

    [tests] Create the directory for graphs

--- tests/initialize_empty.sh
+++ tests/initialize_empty.sh
@@ -5,6 +5,7 @@ rm -rf tests/webpage
 mkdir -p tests/webpage/database/challenges
 mkdir -p tests/webpage/database/users
 mkdir -p tests/webpage/database/games
+mkdir -p tests/webpage/database/graphs
 mkdir -p tests/webpage/icons
 mkdir -p tests/webpage/ssl
 
