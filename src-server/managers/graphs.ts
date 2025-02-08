6f0220df11193ef0b53be665d245f624fcaf5553
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2025-02-08 18:40:55 +0100

    [src-server] Added managers for graphs

---
+++ src-server/managers/graphs.ts
@@ -0,0 +1,62 @@
+/*
+Elo rating for a Chess Club
+Copyright (C) 2023 - 2025  Lluís Alemany Puig
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Full source code of elo-chess-tracker:
+    https://github.com/lluisalemanypuig/elo-chess-tracker
+
+Contact:
+    Lluís Alemany Puig
+    https://github.com/lluisalemanypuig
+*/
+
+import { GameResult } from '../models/game';
+import { Graph, graph_to_file } from '../models/graph/graph';
+import { TimeControlID } from '../models/time_control';
+import { EnvironmentManager } from './environment_manager';
+import { GraphsManager } from './graphs_manager';
+
+export function graph_update(w: string, b: string, result: GameResult, id: TimeControlID): void {
+	let manager = GraphsManager.get_instance();
+	let _g = manager.get_graph(id);
+	if (_g == undefined) {
+		throw new Error(`Graph of time control id '${id}' does not exist.`);
+	}
+	let g = _g as Graph;
+	g.add_edge(w, b, result);
+
+	const graphs_dir = EnvironmentManager.get_instance().get_dir_graphs_time_control(id);
+	graph_to_file(graphs_dir, [w], g);
+}
+
+export function graph_modify_edge(
+	w: string,
+	b: string,
+	old_res: GameResult,
+	new_res: GameResult,
+	id: TimeControlID
+): void {
+	let manager = GraphsManager.get_instance();
+	let _g = manager.get_graph(id);
+	if (_g == undefined) {
+		throw new Error(`Graph of time control id '${id}' does not exist.`);
+	}
+	let g = _g as Graph;
+	g.change_game_result(w, b, old_res, new_res);
+
+	const graphs_dir = EnvironmentManager.get_instance().get_dir_graphs_time_control(id);
+	graph_to_file(graphs_dir, [w], g);
+}
---
+++ src-server/managers/graphs_manager.ts
@@ -0,0 +1,62 @@
+/*
+Elo rating for a Chess Club
+Copyright (C) 2023 - 2025  Lluís Alemany Puig
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Full source code of elo-chess-tracker:
+    https://github.com/lluisalemanypuig/elo-chess-tracker
+
+Contact:
+    Lluís Alemany Puig
+    https://github.com/lluisalemanypuig
+*/
+
+import { Graph } from '../models/graph/graph';
+import { TimeControlID } from '../models/time_control';
+import { search_linear_by_key } from '../utils/searching';
+
+export class GraphsManager {
+	private static instance: GraphsManager;
+
+	constructor() {
+		if (GraphsManager.instance) {
+			return GraphsManager.instance;
+		}
+		GraphsManager.instance = this;
+	}
+
+	static get_instance(): GraphsManager {
+		GraphsManager.instance = GraphsManager.instance || new GraphsManager();
+		return GraphsManager.instance;
+	}
+
+	private graph_list: [TimeControlID, Graph][] = [];
+
+	add_graph(id: TimeControlID, g: Graph): void {
+		const idx = search_linear_by_key(this.graph_list, (pair: [TimeControlID, Graph]): boolean => {
+			return pair[0] == id;
+		});
+		if (idx == -1) {
+			this.graph_list.push([id, g]);
+		}
+	}
+
+	get_graph(id: TimeControlID): Graph | undefined {
+		const idx = search_linear_by_key(this.graph_list, (pair: [TimeControlID, Graph]): boolean => {
+			return pair[0] == id;
+		});
+		return idx != -1 ? this.graph_list[idx][1] : undefined;
+	}
+}
