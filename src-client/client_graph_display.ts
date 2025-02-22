be28fdc8f6f8325ecbed564b3b504f3c1dee8f15
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2025-02-22 09:48:57 +0100

    [html] [src-client] Redesigned pages to display graphs

--- src-client/client_graph_display.ts
+++ src-client/client_graph_display.ts
@@ -8,6 +8,9 @@ import { set_footer_version_number } from './client_utils_version_number';
 import { fill_time_controls } from './client_utils_time_control_select';
 
 let s: Sigma;
+let graph_data: any;
+let server_graph: Graph;
+let graph_loaded: boolean = false;
 
 function resize_viewer() {
 	const viewport_height = window.innerHeight;
@@ -29,7 +32,7 @@ function initialize_sigma() {
 	});
 }
 
-async function display_graph(_event: any) {
+async function load_graph() {
 	const time_control_select = document.getElementById('time_control_select') as HTMLSelectElement;
 	const time_control_id = time_control_select.options[time_control_select.selectedIndex].value;
 
@@ -55,49 +58,118 @@ async function display_graph(_event: any) {
 		headers: { 'Content-type': 'application/json; charset=UTF-8' }
 	});
 
-	const data = await response.json();
-	if (data.r == '0') {
+	graph_data = await response.json();
+	if (graph_data.r == '0') {
+		graph_loaded = false;
 		return;
 	}
 
-	// Create a graph, with various parallel edges:
-	let graph = new Graph({ type: 'directed' });
+	if (graph_loaded) {
+		server_graph.clear();
+	}
+	server_graph = new Graph({ type: 'directed' });
 
-	for (const node of data.nodes) {
-		const name = graph.addNode(node.random_id, { size: node.size, label: node.full_name });
-		graph.setNodeAttribute(name, 'color', 'blue');
+	for (const node of graph_data.nodes) {
+		server_graph.addNode(node.id, { label: node.full_name });
 	}
 
-	for (const edge of data.edges) {
-		const name = graph.addEdge(edge.source, edge.target, { label: edge.label });
-		graph.setEdgeAttribute(name, 'color', 'red');
-		graph.setEdgeAttribute(name, 'size', edge.size);
+	for (const edge of graph_data.edges) {
+		server_graph.addEdge(edge.source, edge.target, { label: edge.label });
 	}
 
-	for (let u of graph.nodeEntries()) {
-		for (let v of graph.outNeighborEntries(u.node)) {
-			const found = graph.hasDirectedEdge(v.neighbor, u.node);
+	for (let u of server_graph.nodeEntries()) {
+		for (let v of server_graph.outNeighborEntries(u.node)) {
+			const found = server_graph.hasDirectedEdge(v.neighbor, u.node);
 			if (found) {
-				graph.setEdgeAttribute(u.node, v.neighbor, 'type', 'curved');
-				graph.setEdgeAttribute(u.node, v.neighbor, 'curvature', 0.25);
+				server_graph.setEdgeAttribute(u.node, v.neighbor, 'type', 'curved');
+				server_graph.setEdgeAttribute(u.node, v.neighbor, 'curvature', 0.25);
 			}
 		}
 	}
 
-	let i = 0;
-	for (let node of graph.nodes()) {
-		graph.setNodeAttribute(node, 'x', i);
-		graph.setNodeAttribute(node, 'y', i * i - i);
-		++i;
-	}
-	const res = forceAtlas2(graph, { iterations: 100 });
-	for (let node of graph.nodes()) {
-		graph.setNodeAttribute(node, 'x', res[node]['x']);
-		graph.setNodeAttribute(node, 'y', res[node]['y']);
+	graph_loaded = true;
+}
+
+function change_node_color(_event: any) {
+	if (!graph_loaded) {
+		return;
 	}
 
+	for (const node of graph_data.nodes) {
+		server_graph.setNodeAttribute(node.id, 'color', 'blue');
+	}
+
+	display_graph();
+}
+
+function change_node_size(_event: any) {
+	if (!graph_loaded) {
+		return;
+	}
+
+	for (const node of graph_data.nodes) {
+		server_graph.setNodeAttribute(node.id, 'size', 2);
+	}
+
+	display_graph();
+}
+
+function change_edge_color(_event: any) {
+	if (!graph_loaded) {
+		return;
+	}
+
+	for (const edge of graph_data.edges) {
+		server_graph.setEdgeAttribute(edge.source, edge.target, 'color', 'red');
+	}
+
+	display_graph();
+}
+
+function change_edge_size(_event: any) {
+	if (!graph_loaded) {
+		return;
+	}
+
+	for (const edge of graph_data.edges) {
+		server_graph.setEdgeAttribute(edge.source, edge.target, 'size', edge.size);
+	}
+
+	display_graph();
+}
+
+function compute_coordinates() {
+	if (!graph_loaded) {
+		return;
+	}
+
+	let i = 0;
+	for (const node of server_graph.nodes()) {
+		server_graph.setNodeAttribute(node, 'x', i);
+		server_graph.setNodeAttribute(node, 'y', i * i - i);
+		++i;
+	}
+	const res = forceAtlas2(server_graph, { iterations: 100 });
+	for (const node of server_graph.nodes()) {
+		server_graph.setNodeAttribute(node, 'x', res[node]['x']);
+		server_graph.setNodeAttribute(node, 'y', res[node]['y']);
+	}
+}
+
+function display_graph() {
+	console.log('Display coordinates...');
+	compute_coordinates();
+	console.log('Clear sigma...');
 	s.clear();
-	s.setGraph(graph);
+	console.log('Set sigma...');
+	s.setGraph(server_graph);
+}
+
+async function load_and_display(_event: any) {
+	console.log('About to load a graph...');
+	await load_graph();
+	console.log('About to display the graph...');
+	display_graph();
 }
 
 window.onresize = resize_viewer;
@@ -109,5 +181,15 @@ window.onload = async function () {
 	initialize_sigma();
 
 	let time_control_select = document.getElementById('time_control_select') as HTMLSelectElement;
-	time_control_select.onchange = display_graph;
+	time_control_select.onchange = load_and_display;
+
+	let node_color_select = document.getElementById('node_color_select') as HTMLSelectElement;
+	node_color_select.onchange = change_node_color;
+	let node_size_select = document.getElementById('node_size_select') as HTMLSelectElement;
+	node_size_select.onchange = change_node_size;
+
+	let edge_color_select = document.getElementById('edge_color_select') as HTMLSelectElement;
+	edge_color_select.onchange = change_edge_color;
+	let edge_size_select = document.getElementById('edge_size_select') as HTMLSelectElement;
+	edge_size_select.onchange = change_edge_size;
 };
