0dd023ff84c044fccb178556e8f4c51a353bcaff
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2025-03-09 10:09:16 +0100

    [src-client] Added licence header

--- src-client/client_graph_display.ts
+++ src-client/client_graph_display.ts
@@ -1,3 +1,28 @@
+/*
+Elo rating for a Chess Club
+Copyright (C) 2023 - 2025  Lluís Alemany Puig
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Full source code of elo-chess-tracker:
+	https://github.com/lluisalemanypuig/elo-chess-tracker
+
+Contact:
+	Lluís Alemany Puig
+	https://github.com/lluisalemanypuig
+*/
+
 import EdgeCurveProgram from '@sigma/edge-curve';
 import Graph from 'graphology';
 import Sigma from 'sigma';
