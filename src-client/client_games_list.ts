6b68b04a7a670d4a068219169b8a6033b643075a
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2025-03-29 15:06:45 +0100

    [src-client] Minor changes

--- src-client/client_games_list.ts
+++ src-client/client_games_list.ts
@@ -129,7 +129,8 @@ function new_cell_button_delete(game_id: string) {
 }
 
 async function fill_games_list_time_control(time_control_id: string) {
-	const val = document.getElementById('table_games')?.getAttribute('value');
+	let table = document.getElementById('table_games') as HTMLTableElement;
+	const val = table.getAttribute('value');
 
 	const query_to_server: string = (() => {
 		if (val == 'all') {
@@ -183,7 +184,6 @@ async function fill_games_list_time_control(time_control_id: string) {
 		new_tbody.appendChild(row);
 	}
 
-	let table = document.getElementById('table_games') as HTMLTableElement;
 	let old_tbody = table.getElementsByTagName('tbody')[0] as HTMLElement;
 	old_tbody.parentNode?.replaceChild(new_tbody, old_tbody);
 }
