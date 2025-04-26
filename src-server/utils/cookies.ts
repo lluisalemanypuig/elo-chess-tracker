2b76426f521b826475a8b3b65edb17fa2e9f9507
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2025-04-26 13:24:38 +0200

    [src-server] [utils] Trying to make cookies work

--- src-server/utils/cookies.ts
+++ src-server/utils/cookies.ts
@@ -23,6 +23,8 @@ Contact:
 	https://github.com/lluisalemanypuig
 */
 
+import { ConfigurationManager } from '../managers/configuration_manager';
+
 // Code adapted from: https://www.w3schools.com/js/js_cookies.asp
 
 /**
@@ -31,20 +33,24 @@ Contact:
  * The object can contain
  * - (*) name: name of the cookie
  * - (*) value: value of the cookie
- * - (?) days: amount of days to expire. Default: '1'
- * - (?) path: path associated to the cookie. Default: '/'
- * - (?) SameSite: (see docs). Default 'Lax'
- * - (?) secure: should the cookie be sent only over https? Default: 'secure'
+ * - (*) days: amount of days to expire. Default: '1'
  *
  * Fields marked with (*) are mandatory; those marked with (?) are optional.
  * @param values
  * @pre Parameter @e values must have entries: 'name' and 'values'.
  */
 export function make_cookie_string(values: any): string {
+	let cookie: string = '';
+
 	// name and value of the cookie
 	const name = values['name'];
 	const value = values['value'];
-	let cookie = encodeURIComponent(name) + '=' + encodeURIComponent(value);
+	cookie += encodeURIComponent(name) + '=' + encodeURIComponent(value);
+
+	cookie += '; Domain=';
+	if (ConfigurationManager.is_production()) {
+		cookie += encodeURIComponent(ConfigurationManager.get_instance().get_domain_name());
+	}
 
 	// time to expire
 	let days = 1;
@@ -56,64 +62,51 @@ export function make_cookie_string(values: any): string {
 	cookie += '; expires=' + d.toUTCString();
 
 	// path
-	let path = '/';
-	if (values['path'] != undefined) {
-		path = values['path'];
-	}
-	cookie += '; path=' + encodeURIComponent(path);
+	cookie += '; path=/';
 
 	// SameSite
-	let same_site = 'Lax';
-	if (values['SameSite'] != undefined) {
-		same_site = values['SameSite'];
-	}
-	cookie += '; samesite=' + encodeURIComponent(same_site);
+	cookie += '; SameSite=Lax';
 
-	// Secure
-	let secure = 'secure';
-	if (values['secure'] != undefined) {
-		secure = values['secure'];
+	// security
+	if (ConfigurationManager.is_production()) {
+		cookie += '; Secure';
 	}
-	cookie += '; ' + encodeURIComponent(secure);
 
 	return cookie;
 }
 
-/*
 /**
- * @brief Retrieve the value of a cookie
- * @param name Name of the cookie to be retrieved
- * @returns A string object, containing the value of the cookie
- //
-export function get_cookie(name: string): string {
-	const _name = name + '=';
-	const ca = document.cookie.split(';');
-	for (let i = 0; i < ca.length; i++) {
-		let c = ca[i];
-		while (c.charAt(0) == ' ') {
-			c = c.substring(1);
-		}
-		if (c.indexOf(_name) == 0) {
-			return decodeURIComponent(c.substring(_name.length, c.length));
-		}
-	}
-	return '';
-}
+ * @brief Constructs a cookie from a JSON object.
+ *
+ * The object can contain
+ * - (*) name: name of the cookie
+ *
+ * Fields marked with (*) are mandatory; those marked with (?) are optional.
+ * @param values
+ * @pre Parameter @e values must have entries: 'name' and 'values'.
+ */
+export function empty_cookie(values: any): string {
+	let cookie: string = '';
 
-export function check_cookie(): void {
-	let user = get_cookie("username");
-	if (user != "") {
-		alert("Welcome again " + user);
+	// name and value of the cookie
+	const name = values['name'];
+	cookie += encodeURIComponent(name) + '=';
+
+	cookie += '; Domain=';
+	if (ConfigurationManager.is_production()) {
+		cookie += encodeURIComponent(ConfigurationManager.get_instance().get_domain_name());
 	}
-	else {
-		user = prompt("Please enter your name:", "") as string;
-		if (user != "" && user != null) {
-			set_cookie({
-				"name" : "username",
-				"value" : user,
-				"days" : "365"
-			});
-		}
+
+	// path
+	cookie += '; path=/';
+
+	// SameSite
+	cookie += '; SameSite=Lax';
+
+	// security
+	if (ConfigurationManager.is_production()) {
+		cookie += '; Secure';
 	}
+
+	return cookie;
 }
-*/
