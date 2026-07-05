e411f1bf70f9e36f65940fec0b61067ea3072d4a
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2026-07-05 12:45:55 +0200

    [app][common] List all entry point routes

---
+++ app/common/routes.ts
@@ -0,0 +1,84 @@
+/*
+Elo rating for a Chess Club
+Copyright (C) 2023 - 2026  Lluís Alemany Puig
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Full source code of elo-chess-tracker:
+    https://github.com/lluisalemanypuig/elo-chess-tracker
+
+Contact:
+    Lluís Alemany Puig
+    https://github.com/lluisalemanypuig
+*/
+
+export const CSS_ALL = '/css/*.css';
+export const VERSION_NUMBER = '/version_number';
+export const FAVICON_ICO = '/favicon.ico';
+export const ICON_LOGIN_PAGE = '/icon/login_page';
+export const ICON_HOME_PAGE = '/icon/home_page';
+export const TITLE_LOGIN_PAGE = '/title/login_page';
+export const TITLE_HOME_PAGE = '/title/home_page';
+export const ROOT = '/';
+export const HOME = '/home';
+export const JS_ALL = '/js/*';
+
+export const QUERY_USER_LIST = '/query/user/list';
+export const QUERY_HTML_USER_LIST = '/query/html/user/list';
+export const QUERY_USER_HOME = '/query/user/home';
+export const QUERY_USER_EDIT = '/query/user/edit';
+export const QUERY_USER_RANKING = '/query/user/ranking';
+export const QUERY_CHALLENGE_RECEIVED = '/query/challenge/received';
+export const QUERY_CHALLENGE_SENT = '/query/challenge/sent';
+export const QUERY_CHALLENGE_PENDING_RESULT = '/query/challenge/pending_result';
+export const QUERY_CHALLENGE_CONFIRM_RESULT_OTHER = '/query/challenge/confirm_result/other';
+export const QUERY_CHALLENGE_CONFIRM_RESULT_SELF = '/query/challenge/confirm_result/self';
+export const QUERY_GAME_LIST_OWN = '/query/game/list/own';
+export const QUERY_GAME_LIST_ALL = '/query/game/list/all';
+export const QUERY_GRAPH_OWN = '/query/graph/own';
+export const QUERY_GRAPH_FULL = '/query/graph/full';
+export const QUERY_HTML_TIME_CONTROLS = '/query/html/time_controls';
+export const QUERY_HTML_TIME_CONTROLS_UNIQUE = '/query/html/time_controls_unique';
+
+export const USER_LOGIN = '/user/login';
+export const USER_LOGOUT = '/user/logout';
+export const USER_CREATE = '/user/create';
+export const USER_EDIT = '/user/edit';
+export const USER_PASSWORD_CHANGE = '/user/password_change';
+
+export const PAGE_USER_CREATE = '/page/user/create';
+export const PAGE_USER_EDIT = '/page/user/edit';
+export const PAGE_USER_PASSWORD_CHANGE = '/page/user/password_change';
+export const PAGE_USER_RANKING = '/page/user/ranking';
+export const PAGE_GAME_CREATE = '/page/game/create';
+export const PAGE_GAME_LIST_OWN = '/page/game/list/own';
+export const PAGE_GAME_LIST_ALL = '/page/game/list/all';
+export const PAGE_GRAPH_OWN = '/page/graph/own';
+export const PAGE_GRAPH_FULL = '/page/graph/full';
+export const PAGE_CHALLENGE = '/page/challenge';
+
+export const GAME_CREATE = '/game/create';
+export const GAME_DELETE = '/game/delete';
+export const GAME_EDIT_TITLE = '/game/edit_title';
+export const GAME_EDIT_RESULT = '/game/edit_result';
+
+export const CHALLENGE_SEND = '/challenge/send';
+export const CHALLENGE_ACCEPT = '/challenge/accept';
+export const CHALLENGE_DECLINE = '/challenge/decline';
+export const CHALLENGE_SET_RESULT = '/challenge/set_result';
+export const CHALLENGE_AGREE = '/challenge/agree';
+export const CHALLENGE_DISAGREE = '/challenge/disagree';
+
+export const RECALCULATE_RATINGS = '/recalculate/ratings';
+export const RECALCULATE_GRAPHS = '/recalculate/graphs';
