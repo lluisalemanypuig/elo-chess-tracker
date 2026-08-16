79e67a543cbe86407e9670d3226906931867d5ff
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2026-08-16 09:59:08 +0200

    feat: added randomization of the symbols allowed for new passwords

--- build/randomize-allowed-symbols.sh
+++ build/randomize-allowed-symbols.sh
@@ -7,20 +7,38 @@ function escape_string {
 	echo $replaceEscaped
 }
 
+# app/server/utils/encrypt.ts
+
 file='app/server/utils/encrypt.ts'
 if [ ! -f $file ]; then
 	echo "    File '$f' does not exist"
 	exit 1
 fi
-raw_symbols_encrypt=$(bun build/random_symbols.ts)
+raw_symbols_encrypt=$(bun build/random-symbols.ts)
 symbols_encrypt=$(echo "$raw_symbols_encrypt" | jq -r '.symbols')
+echo "Replacing with symbols: $symbols_encrypt"
 sed -i "s/\$ALLOWED_SYMBOLS_ENCRYPT/$(escape_string "$symbols_encrypt")/g" $file
+
+# app/server/managers/session.ts
 
 file='app/server/managers/session.ts'
 if [ ! -f $file ]; then
 	echo "    File '$f' does not exist"
 	exit 1
 fi
-raw_symbols_cookies=$(bun build/random_symbols.ts)
+raw_symbols_cookies=$(bun build/random-symbols.ts)
 symbols_cookies=$(echo "$raw_symbols_cookies" | jq -r '.symbols')
+echo "Replacing with symbols: $symbols_encrypt"
 sed -i "s/\$ALLOWED_SYMBOLS_COOKIES/$(escape_string "$symbols_cookies")/g" $file
+
+# app/client/users-new.ts
+
+file='app/client/users-new.ts'
+if [ ! -f $file ]; then
+	echo "    File '$f' does not exist"
+	exit 1
+fi
+raw_symbols_cookies=$(bun build/random-symbols.ts)
+symbols_cookies=$(echo "$raw_symbols_cookies" | jq -r '.symbols')
+echo "Replacing with symbols: $symbols_encrypt"
+sed -i "s/\$ALLOWED_SYMBOLS_RANDOM_PASSWORD/$(escape_string "$symbols_cookies")/g" $file
